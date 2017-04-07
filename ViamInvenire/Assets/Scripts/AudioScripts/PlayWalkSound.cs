8a995418a50b53f221b8afcc7b3cf506d03ba95b
Chandler Waugh <chandler@gbpackerfans.com>
2017-04-06 20:04:50 -0600

    Started making sounds for the walking.

--- ViamInvenire/Assets/Scripts/AudioScripts/PlayWalkSound.cs
+++ ViamInvenire/Assets/Scripts/AudioScripts/PlayWalkSound.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class PlayWalkSound : MonoBehaviour
 {
-    [Header("Grass Sound List")]
-    public List<AudioClip> grassSounds;
-    [Header("Rock Sound List")]
-    public List<AudioClip> rockSounds;
+    [Header("Green Grass Sound List")]
+    public List<AudioClip> greenGrassSounds;
+    [Header("Brown Grass Sound List")]
+    public List<AudioClip> brownGrassSounds;
+    [Header("Solid Stone Sound List")]
+    public List<AudioClip> solidRockSounds;
+    [Header("Gravel Stone Sound List")]
+    public List<AudioClip> gravelRockSounds;
+    [Header("Soft Dirt Sound List")]
+    public List<AudioClip> softDirtSounds;
+    [Header("Hard Dirt Sound List")]
+    public List<AudioClip> hardDirtSounds;
     [Header("Sand Sound List")]
     public List<AudioClip> sandSounds;
 
     private List<List<AudioClip>> SoundsList;
     private int textureIndex;
     private DetermineGroundTexture det;
+    private AudioSource audioSrc;
     private AudioClip soundToPlay;
     private System.Random randSoundIndex;
     private bool movingAndGrounded;
@@ -22,32 +31,38 @@ public class PlayWalkSound : MonoBehaviour
     void Start()
     {
         SoundsList = new List<List<AudioClip>>();
-        SoundsList.Add(grassSounds);
-        SoundsList.Add(rockSounds);
+        SoundsList.Add(greenGrassSounds);
+        SoundsList.Add(brownGrassSounds);
+        SoundsList.Add(solidRockSounds);
+        SoundsList.Add(gravelRockSounds);
+        SoundsList.Add(softDirtSounds);
+        SoundsList.Add(hardDirtSounds);
         SoundsList.Add(sandSounds);
 
+        audioSrc = this.gameObject.GetComponent<AudioSource>();
         det = this.gameObject.GetComponent<DetermineGroundTexture>();
         textureIndex = det.GetIndexOfCurrentTexture();
         randSoundIndex = new System.Random();
         randSoundIndex.Next();
-        //soundToPlay = SoundsList[textureIndex][randSoundIndex.Next(SoundsList[textureIndex].Count)];
+        soundToPlay = SoundsList[textureIndex][randSoundIndex.Next(SoundsList[textureIndex].Count)];
+        audioSrc.clip = soundToPlay;
         movingAndGrounded = this.gameObject.GetComponent<CharacterMotor>().isMovingAndGrounded();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //get index of the texture currently on
         textureIndex = det.GetIndexOfCurrentTexture();
-        //soundToPlay = SoundsList[textureIndex][randSoundIndex.Next(SoundsList[textureIndex].Count)];
+        //get a random sound from collection of sounds representing that texture
+        soundToPlay = SoundsList[textureIndex][randSoundIndex.Next(SoundsList[textureIndex].Count)];
+        //make sure the player is on the ground and moving
         movingAndGrounded = this.gameObject.GetComponent<CharacterMotor>().isMovingAndGrounded();
-        if(movingAndGrounded)
+        //finally make sure the sound isn't already playing
+        if(movingAndGrounded && !audioSrc.isPlaying)
         {
-            this.gameObject.GetComponent<AudioSource>().PlayOneShot(soundToPlay);
-            Debug.Log("This is working.");
-        }
-        else
-        {
-            Debug.Log("See?");
+            audioSrc.clip = soundToPlay;
+            audioSrc.Play();
         }
         //Determine sound based on index standing on.  check index on and see what it has in its name play sound accordingly
     }
