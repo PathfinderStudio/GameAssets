71c3ae23a50e77ba645f9474e44a41a5d2ab5935
Jacob Sivo <jake.takahashi@yahoo.com>
2017-05-03 16:07:31 -0600

    Added in Ryan's tutorial stuff for the main menu. Everything should be good to go.

---
+++ ViamInvenire/Assets/HintImages.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintImages : MonoBehaviour {
+    static int numImages = 5;
+    public Sprite[] hints = new Sprite[numImages];
+    int current = 0;
+    // Use this for initialization
+    void Start () {
+        this.transform.GetChild(0).GetComponent<Image>().sprite = hints[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    void show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void change(int dir)
+    {
+        current += dir;
+        if(current < 0)
+        {
+            current = numImages - 1;
+        }
+        else if(current == numImages)
+        {
+            current = 0;
+        }
+
+        this.transform.GetChild(0).GetComponent<Image>().sprite = hints[current];
+        
+    }
+}
