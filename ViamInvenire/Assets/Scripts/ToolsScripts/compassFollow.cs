bd631e6d4f4ed6691f1e4fa22bc705b0da1b651b
Jacob Sivo <jake.takahashi@yahoo.com>
2017-05-03 19:27:24 -0600

    Added some compass changes.  There is still a bug when facing almost directly south.  Don't know how to fix it yet.

--- ViamInvenire/Assets/Scripts/ToolsScripts/compassFollow.cs
+++ ViamInvenire/Assets/Scripts/ToolsScripts/compassFollow.cs
@@ -1,32 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class compassFollow : MonoBehaviour
 {
-    public GameObject camera;
+    public GameObject buddy;
 
     private GameObject northLocation;
     private bool playerHolding = false;
     private Vector3 thingToRotateTowards;
+    private Vector3 playerFacingVector;
+    private float angleBetween;
+    private float offset;
+    private int sign = -1;
+
 
     // Use this for initialization
     void Start()
     {
         northLocation = GameObject.FindGameObjectWithTag("TrueNorth");
         thingToRotateTowards = northLocation.transform.position - this.transform.position;
-        thingToRotateTowards.x = this.transform.position.x;
+        //thingToRotateTowards.Normalize();
+        playerFacingVector = buddy.transform.forward;
+        angleBetween = Vector3.Angle(thingToRotateTowards, playerFacingVector);
+        offset = 0;
         //playerHolding = false;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        //thingToRotateTowards = northLocation.transform.position - this.transform.position;
+        thingToRotateTowards = northLocation.transform.position - this.transform.position;
+        thingToRotateTowards.Normalize();
+        playerFacingVector = buddy.transform.forward;
+        angleBetween = Quaternion.FromToRotation(thingToRotateTowards, playerFacingVector).eulerAngles.y;
+        
+        Debug.Log("rotateTowards " + thingToRotateTowards);
+        Debug.Log("PlayerVector " + playerFacingVector);
+        angleBetween = 360f - angleBetween;    
+        
+        if(buddy.transform.eulerAngles.y > 120.0f && buddy.transform.eulerAngles.y < 150.0f) //if(playerFacingVector.x < 0.9f && playerFacingVector.x > 0.6f && playerFacingVector.z < -0.5f && playerFacingVector.z > -0.8f)
+        {
+            
+            if(buddy.transform.eulerAngles.y > 135.0f)
+            {
+                //offset = 150.0f - buddy.transform.eulerAngles.y;
+                offset = buddy.transform.eulerAngles.y - 120.0f;
+                offset *= offset / 4;
+            }
+            else
+            {
+                offset = buddy.transform.eulerAngles.y - 120.0f;
+                offset *= offset / 5;
+            }
+            
+            angleBetween = angleBetween - offset;
+        }
+        Debug.Log(angleBetween);
         //thingToRotateTowards.x = this.transform.position.x;
         //this.transform.rotation = Quaternion.LookRotation(thingToRotateTowards, Vector3.forward);
-        //this.transform.localRotation = Quaternion.Euler(new Vector3(0f, yAngle, 0f));
-        this.transform.localRotation = Quaternion.Euler(new Vector3(0f, -camera.transform.rotation.eulerAngles.y + 90, 0f)); // rotates it too point north
+        this.transform.localRotation = Quaternion.Euler(new Vector3(0f, angleBetween + 90f, 0f));
+        //this.transform.localRotation = Quaternion.Euler(new Vector3(0f, -camera.transform.rotation.eulerAngles.y + 90, 0f)); // rotates it too point north
     }
 
     public void itemPickedUp(bool input)
